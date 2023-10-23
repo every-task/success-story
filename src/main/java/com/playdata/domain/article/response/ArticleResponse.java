f8a420427bb6679bb61a7c1919320b98af59253b
Amy <wlgpek90@naver.com>
2023-10-23 17:34:25 +0900

    구현 feat: comment 순환참조 끊기위한 로직 구현 #1

--- src/main/java/com/playdata/domain/article/response/ArticleResponse.java
+++ src/main/java/com/playdata/domain/article/response/ArticleResponse.java
@@ -1,17 +1,21 @@
 package com.playdata.domain.article.response;
 
+import com.playdata.domain.article.dto.ArticleDto;
 import com.playdata.domain.article.entity.Article;
+import com.playdata.domain.comment.dto.CommentDto;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
-public class ArticleResponse {
-    private Long id;
-    private String title;
-    private String content;
+public class ArticleResponse extends ArticleDto {
+    private List<CommentDto> comments;
 
     public ArticleResponse(Article article) {
-        this.id = article.getId();
-        this.title = article.getTitle();
-        this.content = article.getContent();
+        super(article);
+        comments = article.getComments()
+                .stream()
+                .map(CommentDto::new)
+                .toList();
     }
 }
