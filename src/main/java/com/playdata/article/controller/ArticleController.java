ed0251e854b1f5bb72b6a613a605b4a5677d1426
Amy <wlgpek90@naver.com>
2023-10-23 12:43:39 +0900

    feat: Article kafka id 조회 및 전체 조회 로직 구현 #1

--- src/main/java/com/playdata/article/controller/ArticleController.java
+++ src/main/java/com/playdata/article/controller/ArticleController.java
@@ -1,12 +1,13 @@
 package com.playdata.article.controller;
 
 import com.playdata.article.service.ArticleService;
+import com.playdata.domain.article.entity.Article;
 import com.playdata.domain.article.request.ArticleRequest;
+import com.playdata.domain.article.response.ArticleResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,5 +18,16 @@ public class ArticleController {
     @PostMapping
     public void save(@RequestBody ArticleRequest articleRequest) {
         articleService.save(articleRequest);
+    }
+
+    @GetMapping
+    public List<ArticleResponse> getAll() {
+        return articleService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ArticleResponse getArticleById(@PathVariable Long id) {
+        Article article = articleService.findById(id);
+        return new ArticleResponse(article);
     }
 }
--- src/main/java/com/playdata/article/service/ArticleService.java
+++ src/main/java/com/playdata/article/service/ArticleService.java
@@ -4,10 +4,15 @@ import com.playdata.domain.article.entity.Article;
 import com.playdata.domain.article.kafka.ArticleKafka;
 import com.playdata.domain.article.repository.ArticleRepository;
 import com.playdata.domain.article.request.ArticleRequest;
+import com.playdata.domain.article.response.ArticleResponse;
 import com.playdata.kafka.SuccessProducer;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,4 +25,14 @@ public class ArticleService {
         Article save = articleRepository.save(articleRequest.toEntity());
         successProducer.send(ArticleKafka.of(save));
     }
+
+    public List<ArticleResponse> findAll() {
+        List<Article> all = articleRepository.findAll();
+        return all.stream().map(ArticleResponse::new).toList();
+    }
+
+    public Article findById(Long id) {
+        return articleRepository.findById(id).orElseThrow(() -> new NoSuchElementException("id 못찾음"));
+    }
+
 }
