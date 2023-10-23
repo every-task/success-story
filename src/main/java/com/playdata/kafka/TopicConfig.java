b55f6e4f809b962586d2853424fae21555874e77
Amy <wlgpek90@naver.com>
2023-10-23 12:38:09 +0900

    feat: 카프카 발행 및 구독 필요한 토픽 구성 #1

--- src/main/java/com/playdata/kafka/TopicConfig.java
+++ src/main/java/com/playdata/kafka/TopicConfig.java
@@ -7,16 +7,21 @@ import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class TopicConfig {
-    public final static String success = "success-topic";
+    public final static String SUCCESS = "success-story"; //내가 발행한 토픽
+    public final static String MEMBER = "member"; //내가 구독한 토픽
 
     @Bean
     public NewTopic success() {
-//        return new NewTopic(topic1, 1, (short) 1);
         return TopicBuilder
-                .name(success)
+                .name(SUCCESS)
                 .partitions(1)
                 .replicas(1)
                 .build();
+    }
+
+    @Bean
+    public NewTopic member() {
+        return new NewTopic(MEMBER, 1, (short) 1);
     }
 
 
