bcede59afcfcb3f4d21943b5a00ff74e9b6cfe92
Amy <wlgpek90@naver.com>
2023-10-24 11:51:22 +0900

    refactor: @AllArgsConstructor           @NoArgsConstructor 추가(에러가 나서) #1

--- src/main/java/com/playdata/domain/task/dto/TaskDto.java
+++ src/main/java/com/playdata/domain/task/dto/TaskDto.java
@@ -2,9 +2,14 @@ package com.playdata.domain.task.dto;
 
 import com.playdata.domain.task.entity.Period;
 import com.playdata.domain.task.entity.Task;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
-
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TaskDto {
     private UUID id;
     private String content;
