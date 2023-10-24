efd59cb04717394ca5d4771061096c69d2ac64f6
Amy <wlgpek90@naver.com>
2023-10-24 15:11:55 +0900

    refactor: comment 로직 수정 #3

--- src/main/java/com/playdata/domain/comment/request/CommentRequest.java
+++ src/main/java/com/playdata/domain/comment/request/CommentRequest.java
@@ -1,12 +1,19 @@
 package com.playdata.domain.comment.request;
 
+import com.playdata.domain.article.entity.Article;
 import com.playdata.domain.comment.entity.Comment;
+import com.playdata.domain.member.entity.Member;
 
-public record CommentRequest(String content) {
+import java.util.UUID;
 
-    public Comment toEntity() {
+public record CommentRequest(String content, Article article) {
+
+    public Comment toEntity(UUID memberId, Long articleId) {
         return Comment.builder()
                 .content(content)
+                .article(article)
+                .member(Member.builder().id(memberId).build())
+                .article(Article.builder().id(articleId).build())
                 .build();
     }
 }
