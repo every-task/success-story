25f413d83e09af2cf88fb862dfce1a87a28773b9
Amy <wlgpek90@naver.com>
2023-10-25 15:57:47 +0900

    refactor: CustomArticleRepository 상속 #14

--- src/main/java/com/playdata/domain/article/repository/ArticleRepository.java
+++ src/main/java/com/playdata/domain/article/repository/ArticleRepository.java
@@ -3,5 +3,5 @@ package com.playdata.domain.article.repository;
 import com.playdata.domain.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends JpaRepository<Article, Long>, CustomArticleRepository {
 }
