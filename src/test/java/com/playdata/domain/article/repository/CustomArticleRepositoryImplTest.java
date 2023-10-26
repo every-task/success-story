3b4cbd6cf2481bca0bbba3fd881d8de1d89d3678
Amy <wlgpek90@naver.com>
2023-10-26 11:51:45 +0900

    refactor: 오류나길래 Transactional 걸었음 #18

--- src/test/java/com/playdata/domain/article/repository/CustomArticleRepositoryImplTest.java
+++ src/test/java/com/playdata/domain/article/repository/CustomArticleRepositoryImplTest.java
@@ -7,9 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+@Transactional
 @SpringBootTest
 class CustomArticleRepositoryImplTest {
     @Autowired
