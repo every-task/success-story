88f278d6902ed22ede034fd143b88103687242ce
Amy <wlgpek90@naver.com>
2023-10-31 11:33:23 +0900

    refactor: 의미없는 수정 #18

--- src/main/java/com/playdata/config/JwtService.java
+++ src/main/java/com/playdata/config/JwtService.java
@@ -1,14 +1,9 @@
 package com.playdata.config;
 
-import com.playdata.domain.member.entity.Member;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 @Service
