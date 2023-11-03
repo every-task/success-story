ab2aa99d121a554ded9d1af3b7181d632571e28d
Amy <wlgpek90@naver.com>
2023-11-03 11:27:21 +0900

    feat : 예외 처리를 위해 추가 #31

---
+++ src/main/java/com/playdata/exception/CustomControllerAdvice.java
@@ -0,0 +1,20 @@
+package com.playdata.exception;
+
+import com.playdata.exception.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class CustomControllerAdvice {
+
+    @ExceptionHandler(NotCorrectMemberException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorResponse notCorrectMemberExceptionHandler(NotCorrectMemberException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+}
---
+++ src/main/java/com/playdata/exception/NotCorrectMemberException.java
@@ -0,0 +1,9 @@
+package com.playdata.exception;
+
+public class NotCorrectMemberException extends RuntimeException {
+
+    public NotCorrectMemberException(String message) {
+        super(message);
+    }
+
+}
