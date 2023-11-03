f1e95cd38267c1b8052dc3b43e2392b2cd69da98
Amy <wlgpek90@naver.com>
2023-11-03 12:08:25 +0900

    refactor: 게시글 수정시 카테고리도 수정되어야 해서 카테고리 추가 #28

--- src/main/java/com/playdata/domain/article/request/ArticleUpdateRequest.java
+++ src/main/java/com/playdata/domain/article/request/ArticleUpdateRequest.java
@@ -1,6 +1,6 @@
 package com.playdata.domain.article.request;
+import com.playdata.domain.article.entity.Category;
 
-
-public record ArticleUpdateRequest(String title, String content)  {
+public record ArticleUpdateRequest(String title, String content, Category category)  {
 
 }
