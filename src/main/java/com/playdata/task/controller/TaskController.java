d294e8e74d2422c753fc7e61b154e59c31a6ee32
Amy <wlgpek90@naver.com>
2023-11-07 15:48:12 +0900

    feat : Task 업데이트를 위해 추가  #34

--- src/main/java/com/playdata/task/controller/TaskController.java
+++ src/main/java/com/playdata/task/controller/TaskController.java
@@ -1,9 +1,13 @@
 package com.playdata.task.controller;
 
+import com.playdata.domain.task.kafka.TaskUpdateKafka;
+import com.playdata.domain.task.request.TaskUpdateRequest;
+import com.playdata.domain.task.response.TaskResponse;
 import com.playdata.task.service.TaskService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,4 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class TaskController {
     private final TaskService taskService;
 
+    @PutMapping("/task/{id}")
+    public TaskResponse updateTask(@PathVariable UUID id, @RequestBody TaskUpdateRequest taskUpdateRequest) {
+        return taskService.updateTask(id, taskUpdateRequest);
+    }
 }
