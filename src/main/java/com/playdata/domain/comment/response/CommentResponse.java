10691bed11688c1e259059fa092b69f92064f05e
Amy <wlgpek90@naver.com>
2023-11-08 17:55:26 +0900

    refactor : 필요 없어서 삭제 #43

--- src/main/java/com/playdata/domain/comment/response/CommentResponse.java
+++ src/main/java/com/playdata/domain/comment/response/CommentResponse.java
@@ -1,16 +1,13 @@
 package com.playdata.domain.comment.response;
 
-import com.playdata.domain.article.dto.ArticleDto;
 import com.playdata.domain.comment.dto.CommentDto;
 import com.playdata.domain.comment.entity.Comment;
 import lombok.Getter;
 
 @Getter
 public class CommentResponse extends CommentDto {
-    private ArticleDto article;
 
     public CommentResponse(Comment comment) {
         super(comment);
-        article = new ArticleDto(comment.getArticle());
     }
 }
