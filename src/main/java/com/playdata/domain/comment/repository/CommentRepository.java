8f6f1cfb588bdb2f81cdfcc4f7d1f2c50bd766bb
Amy <wlgpek90@naver.com>
2023-11-08 17:55:15 +0900

    refactor : 필요 없어서 삭제 #43

--- src/main/java/com/playdata/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/playdata/domain/comment/repository/CommentRepository.java
@@ -3,8 +3,6 @@ package com.playdata.domain.comment.repository;
 import com.playdata.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-public interface CommentRepository extends JpaRepository<Comment,Long> {
-    List<Comment> findAllByArticleId(Long ArticleId);
 }
