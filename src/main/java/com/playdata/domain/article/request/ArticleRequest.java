35a0d73dea5fe2c8358da6b42cc7a7abaa7a3acc
Amy <wlgpek90@naver.com>
2023-11-10 11:35:16 +0900

    refactor : Dto를 통해서가 아닌 직접 받을 수 있도록 수정 작성 #43

--- src/main/java/com/playdata/domain/article/request/ArticleRequest.java
+++ src/main/java/com/playdata/domain/article/request/ArticleRequest.java
@@ -1,27 +1,31 @@
 package com.playdata.domain.article.request;
 
-import com.playdata.domain.article.dto.ArticleDto;
 import com.playdata.domain.article.entity.Article;
+import com.playdata.domain.article.entity.Category;
 import com.playdata.domain.member.entity.Member;
-import com.playdata.domain.task.dto.TaskDto;
 import com.playdata.domain.task.entity.Task;
+import com.playdata.domain.task.request.TaskRequest;
 
 import java.util.List;
 import java.util.UUID;
 
-public record ArticleRequest(ArticleDto article,
-                              List<TaskDto> tasks) {
+public record ArticleRequest(List<TaskRequest> tasks, String title, String content, Category category) {
 
     public Article toEntityArticle(UUID memberId) {
         return Article.builder()
-                .title(article.getTitle())
-                .content(article.getContent())
-                .category(article.getCategory())
+                .title(title)
+                .content(content)
+                .category(category)
                 .member(Member.builder().id(memberId).build())
                 .build();
     }
 
     public List<Task> toEntityTasks(Article article) {
-        return tasks.stream().map(t -> new Task(null, t.getContent(), t.getPeriod(),article)).toList();
+        return tasks.stream()
+                .map(taskRequest -> Task.builder()
+                        .content(taskRequest.content())
+                        .period(taskRequest.period())
+                        .build())
+                .toList();
     }
 }
