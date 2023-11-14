ed0f147bacf55563cef9ab9323172449211ee9bc
Amy <wlgpek90@naver.com>
2023-11-14 14:45:04 +0900

    refactor : Test code 주석 #45

--- src/test/java/com/playdata/article/service/ArticleServiceTest.java
+++ src/test/java/com/playdata/article/service/ArticleServiceTest.java
@@ -21,35 +21,35 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @Transactional
 class ArticleServiceTest {
-    @Autowired
-    ArticleRepository articleRepository;
-    @Autowired
-    ArticleService articleService;
-    @Autowired
-    MemberRepository memberRepository;
-    Article save;
-    UUID id = UUID.randomUUID();
-
-    @BeforeEach
-    void init(){
-        Member member = Member.builder().id(id).build();
-        memberRepository.save(member);
-        Article article = Article.builder().member(member).title("test").content("content").category(Category.HEALTH).build();
-        save = articleRepository.save(article);
-    }
-
-    @Test
-    void deleteArticle() {
-        TokenInfo info = TokenInfo.builder().id(id).build();
-        articleService.deleteArticle(info, save.getId());
-        Article article = articleRepository.findById(save.getId()).orElseThrow();
-        assertEquals(save.getId(),article.getId());
-        assertTrue(article.isDeleted());
-        assertEquals(
-                articleRepository.findAllByCondition(
-                        PageRequest.of(0,5),
-                        ArticleCondition.builder().build())
-                        .getTotalElements(),
-                articleRepository.findAll().size()-1);
-    }
+//    @Autowired
+//    ArticleRepository articleRepository;
+//    @Autowired
+//    ArticleService articleService;
+//    @Autowired
+//    MemberRepository memberRepository;
+//    Article save;
+//    UUID id = UUID.randomUUID();
+//
+//    @BeforeEach
+//    void init(){
+//        Member member = Member.builder().id(id).build();
+//        memberRepository.save(member);
+//        Article article = Article.builder().member(member).title("test").content("content").category(Category.HEALTH).build();
+//        save = articleRepository.save(article);
+//    }
+//
+//    @Test
+//    void deleteArticle() {
+//        TokenInfo info = TokenInfo.builder().id(id).build();
+//        articleService.deleteArticle(info, save.getId());
+//        Article article = articleRepository.findById(save.getId()).orElseThrow();
+//        assertEquals(save.getId(),article.getId());
+//        assertTrue(article.isDeleted());
+//        assertEquals(
+//                articleRepository.findAllByCondition(
+//                        PageRequest.of(0,5),
+//                        ArticleCondition.builder().build())
+//                        .getTotalElements(),
+//                articleRepository.findAll().size()-1);
+//    }
 }
