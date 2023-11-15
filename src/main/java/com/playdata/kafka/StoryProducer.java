ecd628558509ae26845d5cefcc813dd8763ceef3
Amy <wlgpek90@naver.com>
2023-11-15 09:54:26 +0900

    refactor : Action 을 추가해서 kafka 발행 #45

--- src/main/java/com/playdata/kafka/StoryProducer.java
+++ src/main/java/com/playdata/kafka/StoryProducer.java
@@ -1,6 +1,9 @@
 package com.playdata.kafka;
 
+import com.playdata.domain.article.entity.Article;
 import com.playdata.domain.article.kafka.ArticleKafka;
+import com.playdata.domain.task.dto.TaskDto;
+import com.playdata.exception.PublishingFailedException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -8,6 +11,7 @@ import org.springframework.kafka.support.SendResult;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @Component
@@ -15,21 +19,32 @@ import java.util.concurrent.CompletableFuture;
 @Slf4j
 public class StoryProducer {
     private final KafkaTemplate<String, ArticleKafka> kafkaTemplate;
+
     @Async
-    public void send(ArticleKafka articleKafka) {
-        CompletableFuture<SendResult<String, ArticleKafka>> resultCompletableFuture =
-                kafkaTemplate.send(TopicConfig.STORY, articleKafka);
-
-        if(resultCompletableFuture.isCompletedExceptionally()){
-            throw new RuntimeException("발행 실패");}
-
-        resultCompletableFuture
-                .thenAccept(result ->
-                        System.out.println("send After "
-                                + articleKafka + " "
-                                + result.getRecordMetadata()
-                                .offset()));
-        log.info("send={}",articleKafka);
+    public void sendCreate(Article article, List<TaskDto> tasks) {
+        send(article,tasks, "create");
+    }
+    @Async
+    public void sendUpdate(Article article, List<TaskDto> tasks) {
+        send(article,tasks,"update");
+    }
+    @Async
+    public void sendDelete(Article article, List<TaskDto> tasks) {
+        send(article,tasks, "delete");
     }
 
+    private void send(Article article, List<TaskDto> tasks, String action) {
+        ArticleKafka articleKafka = ArticleKafka.of(article, tasks, action);
+        CompletableFuture<SendResult<String, ArticleKafka>> resultFuture =
+                kafkaTemplate.send(TopicConfig.STORY, articleKafka);
+        resultFuture.whenComplete((result, e) -> {
+            if (e == null) {
+                log.info("Send success: {} Offset: {}",
+                        articleKafka, result.getRecordMetadata().offset());
+            } else {
+                log.error("Send failed: {}", articleKafka);
+                throw new PublishingFailedException("Publishing failed");
+            }
+        });
+    }
 }
