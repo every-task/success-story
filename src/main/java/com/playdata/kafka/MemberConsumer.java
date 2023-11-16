00cc84b0f94aae12944c2f86c27d37f76cb563e9
Amy <wlgpek90@naver.com>
2023-11-16 14:48:50 +0900

    refactor : Consumer 에서 Action 받을 수 있게 수정 #50

--- src/main/java/com/playdata/kafka/MemberConsumer.java
+++ src/main/java/com/playdata/kafka/MemberConsumer.java
@@ -1,10 +1,14 @@
 package com.playdata.kafka;
 
+import com.playdata.domain.member.entity.Member;
 import com.playdata.domain.member.kafka.MemberKafkaData;
 import com.playdata.domain.member.repository.MemberRepository;
+import com.playdata.exception.NotCorrectMemberException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
@@ -13,11 +17,17 @@ public class MemberConsumer {
 
     @KafkaListener(topics = TopicConfig.MEMBER)
     public void listen(MemberKafkaData data) {
-        memberRepository.save(data.ToEntity());
+        if (data.action().equals("CREATE") || (data.action().equals("UPDATE"))) {
+            memberRepository.save(data.toEntity());
+        } else {
+            Member member = memberRepository.findById(UUID.fromString(data.id()))
+                    .orElseThrow(() -> new NotCorrectMemberException("Not Correct Member, memberId = {%s}".formatted(data.id())));
+            member.delete();
+        }
     }
 
     @KafkaListener(topics = TopicConfig.topicDLT)
     public void dltListen(byte[] in) {
-        System.out.println("dltListen"+new String(in)); //수정 예정 아직 뭐로 할지 결정하지 못함
+        System.out.println("dltListen" + new String(in)); //수정 예정 아직 뭐로 할지 결정하지 못함
     }
 }
