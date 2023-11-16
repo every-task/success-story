f4025d95698e527619bb72d51402c238191becd1
Amy <wlgpek90@naver.com>
2023-11-16 09:43:11 +0900

    refactor :  member 수정 #50

--- src/main/java/com/playdata/domain/article/response/ArticleAllResponse.java
+++ src/main/java/com/playdata/domain/article/response/ArticleAllResponse.java
@@ -3,7 +3,9 @@ package com.playdata.domain.article.response;
 import com.playdata.domain.article.dto.ArticleDto;
 import com.playdata.domain.article.entity.Article;
 import com.playdata.domain.member.dto.MemberDto;
+import lombok.Getter;
 
+@Getter
 public class ArticleAllResponse extends ArticleDto {
     private MemberDto member;
 
