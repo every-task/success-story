642c7eb8479b3478fd76244a4b6ea62d2b85badb
Amy <wlgpek90@naver.com>
2023-11-21 12:33:51 +0900

    refactor : member 추가 #63

--- src/main/java/com/playdata/domain/comment/response/CommentResponse.java
+++ src/main/java/com/playdata/domain/comment/response/CommentResponse.java
@@ -2,12 +2,15 @@ package com.playdata.domain.comment.response;
 
 import com.playdata.domain.comment.dto.CommentDto;
 import com.playdata.domain.comment.entity.Comment;
+import com.playdata.domain.member.dto.MemberDto;
 import lombok.Getter;
 
 @Getter
 public class CommentResponse extends CommentDto {
+    private MemberDto member;
 
     public CommentResponse(Comment comment) {
         super(comment);
+        member = new MemberDto(comment.getMember());
     }
 }
