69edc197fbc8c7af648d58e83e73c45016003df1
Amy <wlgpek90@naver.com>
2023-11-21 14:43:52 +0900

    refactor : createdAt 추가

--- src/main/java/com/playdata/domain/comment/dto/CommentDto.java
+++ src/main/java/com/playdata/domain/comment/dto/CommentDto.java
@@ -4,15 +4,19 @@ import com.playdata.domain.comment.entity.Comment;
 import com.playdata.domain.member.dto.MemberDto;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class CommentDto {
     private Long id;
     private String content;
     private MemberDto member;
+    protected LocalDateTime createdAt;
 
     public CommentDto(Comment comment) {
         this.id = comment.getId();
         this.content = comment.getContent();
+        this.createdAt = comment.getCreatedAt();
         member = new MemberDto(comment.getMember());
     }
 }
