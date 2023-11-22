db779881e475d6654e7a8f783d6ff11c6827dbe3
Amy <wlgpek90@naver.com>
2023-11-22 16:10:58 +0900

    refactor : JPA쿼리를 query dsl 쿼리 로 변경  #54

--- src/main/java/com/playdata/domain/article/repository/CustomArticleRepository.java
+++ src/main/java/com/playdata/domain/article/repository/CustomArticleRepository.java
@@ -1,6 +1,7 @@
 package com.playdata.domain.article.repository;
 
 import com.playdata.domain.article.dto.ArticleCondition;
+import com.playdata.domain.article.entity.Article;
 import com.playdata.domain.article.response.ArticleAllResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -10,5 +11,9 @@ public interface CustomArticleRepository {
     Page<ArticleAllResponse> findAllByCondition(
             PageRequest request,
             ArticleCondition condition
+    );
+
+    Article getArticleByIdFetchComment(
+            Long id
     );
 }
