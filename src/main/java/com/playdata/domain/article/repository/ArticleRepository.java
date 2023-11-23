9712069a65ce6799346fe5186023b10980b22a97
Amy <wlgpek90@naver.com>
2023-11-23 15:50:44 +0900

    refactor : comment 분리로 인해 쿼리 수정 #54

--- src/main/java/com/playdata/domain/article/repository/ArticleRepository.java
+++ src/main/java/com/playdata/domain/article/repository/ArticleRepository.java
@@ -4,10 +4,15 @@ import com.playdata.domain.article.entity.Article;
 import com.playdata.domain.article.response.ArticleResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ArticleRepository extends JpaRepository<Article, Long>, CustomArticleRepository {
     @Query("select a from Article a where a.isDeleted = false order by a.view desc, a.createdAt desc limit :pageSize")
     List<ArticleResponse> findAllByViewAndCreatedAt(int pageSize);
+
+    @Query("select a from Article a where a.isDeleted = false and a.id =:id order by a.createdAt desc ")
+    Optional<Article> findArticleById(@Param("id")Long id);
 }
